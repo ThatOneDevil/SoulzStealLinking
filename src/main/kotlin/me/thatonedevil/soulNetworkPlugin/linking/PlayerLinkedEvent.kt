a7d250f0080763a799762b48ac615ac864446f0d
ThatOneDevil <62218606+ThatOneDevil@users.noreply.github.com>
2025-05-08 16:45:29 +0100

    Add documentation to PlayerLinkedEvent class
    Document the purpose and usage of the PlayerLinkedEvent class, including parameters and a usage note regarding skript-reflect. Also added the @Suppress annotation for unused warnings.

--- src/main/kotlin/me/thatonedevil/soulNetworkPlugin/linking/PlayerLinkedEvent.kt
+++ src/main/kotlin/me/thatonedevil/soulNetworkPlugin/linking/PlayerLinkedEvent.kt
@@ -4,6 +4,16 @@ import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
+
+/**
+ * This event is called when a player is linked or unlinked.
+ * This event is only called using skript-reflect thus it is not used in the plugin.
+ *
+ * @param player The player that was linked or unlinked.
+ * @param isLinked True if the player was linked, false if the player was unlinked.
+ * @param uuid The uuid of the player that was linked or unlinked.
+ */
+@Suppress("unused")
 class PlayerLinkedEvent (
     val player: Player,
     val isLinked: Boolean,
